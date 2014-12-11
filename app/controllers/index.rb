b0c287e01937beccdefc4be878b9b54213cbb204
CJ Johnson <Fermentedbeats@gmail.com>
2014-12-11 15:06:10 -0800

    update index

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,60 +1,59 @@
 get '/' do
-  erb :login, layout :false
+  erb :login
   #erb :index
 end
 
 get '/profile/:user_name' do
   @user = User.find(params[:user_name])
-  @tweets = Tweet.all.where(user_id: @user.id)
   erb :profile
 end
 
 # for header new tweets in profile & timeline
-post '/profile/:user_name' do
-  @tweet = Tweet.create(user_name: params[:user_name], content: params[:content])
+post '/tweet' do
+  @user = User.find(params[:user_name])
+  @tweet = Tweet.create(user_id: @user.id, content: params[:content])
   redirect'/profile/:user_name'
 end
 
-# for header new tweets in profile & timeline
-post '/timeline/:user_name' do
-  @tweet = Tweet.create(user_name: params[:user_name], content: params[:content])
-  redirect'/timeline/:user_name'
-end
+# # for header new tweets in profile & timeline
+# post '/timeline/:user_name' do
+#   @user = User.find(params[:user_name])
+#   @tweet = Tweet.create(user_id: @user.id, content: params[:content])
+#   redirect'/timeline/:user_name'
+# end
 
-#TWEET IS NOT A SEPARATE PAGE. BUILD DELETE INTO PROFILE AND TIMELINE
-get '/tweet/:user_name' do
-  @tweet = Tweet.find_by user_name: params[:user_name]
-  #delete option on this page
-  #link back to timeline & profile on this page
-  erb :tweet
-end
 
-delete '/tweet/:user_name' do
-  Tweet.destroy(params[:user_name])
+# get '/tweet/:user_name' do
+#   @tweet = Tweet.find_by user_name: params[:user_name]
+#   #delete option on this page
+#   #link back to timeline & profile on this page
+#   erb :tweet
+# end
+
+delete '/profile/:user_name' do
+  Tweet.destroy(params[:id])
   redirect '/profile/:user_name'
 end
 
-#FOLLOWERS IS NOT A SEPARATE PAGE. IT'S A SIDEBAR ON PROFILE
 get '/follow/:user_name' do
-
   #both your followers & who you follow
   @user = User.find(params[:user_name])
-  @followers_ids = Follow.where(user: @user.id)
-  @followers_objects = @followers.followed_by.user
+  @followers = Following.where(user: @user.id)
+  @followers = @followers.followed_by.user
   #returns list of objects for specific user.. Erb will get followers ids
-  @following_ids = Follow.where(followed_by: @user.id)
-  @following_objects = @following.user.user
+  @following = Following.where(followed_by: @user.id)
+  @following = @following.user.user
+  erb :follow
 end
 
 get '/timeline/:user_name' do
-  @user = User.find(params[:user_name])
-  @follow_objects = Follow.where(user: @user.id)
-  @user_ids = @follow_objects.followed_by
+  @followers = Following.where(user: @user.id)
+  @user_ids = @followers.followed_by
   # @tweets = @user_objects.user_id.tweet
 
   @tweet_objects = []
-  @user_ids.each { |user_id| @tweet_objects << Tweet.where(:user_id = user_id)
-
+  @user_ids.each {|user_id| @tweet_objects << Tweet.where(user: user_id)}
+  @tweet_objects = @tweet_objects.sort_by{|tweet_object| tweet_object.created_at}.reverse
   erb :timeline
 end
 
@@ -63,10 +62,9 @@ end
 # ============SESSIONS
 get '/secret' do
   if !logged_in?
-    redirect '/'
-  else
     redirect '/timeline/:user_name'
   end
+  erb :secret
 end
 
 post '/signup' do
